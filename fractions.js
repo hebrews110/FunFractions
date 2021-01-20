36db5838716d90d994987f5d5e800f1787fbf379
Themba Dube <embeddedthemba@gmail.com>
2021-01-20 15:40:48 -0500

    print mixed component in fractionVal debug

--- fractions.js
+++ fractions.js
@@ -112,8 +112,7 @@ function getRandomNonPrime(min, max) {
 }
 
 function fractionVal(array) {
-    console.log(array[0]);
-    console.log(array[1]);
+    console.log(array[2] + " " + array[0] + " / " + array[1]);
     return (array[0] / array[1]) + array[2];
 }
 
